c5efe8cbb8643841f1832fa152ab13012549cfb9
ThASh42 <thashmail42@gmail.com>
2024-02-13 13:56:39 +0200

    Add mail sending functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Send new email
+  document.querySelector("#compose-form").onsubmit = send_email;
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +33,28 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email() {
+  
+  // Form values
+  const form_recipients = document.querySelector('#compose-recipients').value;
+  const form_subject = document.querySelector('#compose-recipients').value;
+  const form_body = document.querySelector('#compose-body').value;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: form_recipients,
+      subject: form_subject,
+      body: form_body,
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    console.log(result);
+    load_mailbox('inbox');
+  });
+  return false;
 }
